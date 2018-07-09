996fc8354a43895abf586434db899d369d05c603
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-09 21:46:51 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,52 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
-end
+require 'pry'
 
+def get_first_name_of_season_winner(data, season)
+      data[season].each do |contestant|
+            if contestant.values.include?("Winner")
+             return contestant["name"].split.first
+           end 
+      end 
+ end 
+  
 def get_contestant_name(data, occupation)
-  # code here
+  data.values.each do |contestant|
+    contestant.each do |info|
+      if info.values.include?(occupation)
+        return info["name"]
+      end 
+    end 
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.values.each do |contestant|
+    contestant.each do |info|
+      if info.values.include?(hometown)
+        count += 1
+      end
+    end 
+  end 
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.values.each do |contestant|
+      contestant.each do |info|
+        if info.values.include?(hometown)
+          return info["occupation"]
+        end 
+      end
+    end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages_total=[]
+  contestant_number=0
+  count=0
+  data[season].each do |contestant|
+    count += contestant["age"].to_i
+    contestant_number +=1
+  end
+  return ((count.to_f)/contestant_number).round
 end
